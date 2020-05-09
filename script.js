ca5c10c030f98cf794931c15a34100b83e4bdf00
Kumar Sambhav <krsambhav16@gmail.com>
2020-05-10 02:45:59 +0530

    .

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function changeMode(){
     var bodyClass = document.body.className;
     document.body.className = bodyClass == 'light' ? 'dark' : 'light';
+    var modeChanger = document.getElementById('mode');
+    modeChanger.className = modeChanger.className == 'fas fa-moon' ? 'fas fa-sun' : 'fas fa-moon';
 }
